fc3ac5064184f1b783a6d1215e1c0c801a30bcab
Jeymen <jeymen11@outlook.com>
2023-05-19 20:32:56 +0300

    feat: add better pass validation

--- script.js
+++ script.js
@@ -7,18 +7,19 @@ inputForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const emailPattern = /^[\w.-]+@[\w.-]+\.\w+$/;
+  const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   const isEmailValid = emailPattern.test(emailInput.value);
-  const isPasswordSecure = passwordInput.value.length >= 8 && confirmPasswordInput.value.length >= 8;
+  const isPasswordSecure = passwordPattern.test(passwordInput.value);
   const doPasswordsMatch = passwordInput.value === confirmPasswordInput.value && isPasswordSecure;
 
   emailInput.classList.toggle("success", isEmailValid);
   emailInput.classList.toggle("error", !isEmailValid);
 
-  passwordInput.classList.toggle("succes`s", isPasswordSecure);
+  passwordInput.classList.toggle("success", isPasswordSecure);
   passwordInput.classList.toggle("error", !isPasswordSecure);
   confirmPasswordInput.classList.toggle("success", isPasswordSecure);
   confirmPasswordInput.classList.toggle("error", !isPasswordSecure);
-
+ 
   passwordInput.classList.toggle("error", !doPasswordsMatch);
   confirmPasswordInput.classList.toggle("error", !doPasswordsMatch);
   passwordInput.classList.toggle("success", doPasswordsMatch);
